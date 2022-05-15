6fd73bb4dbf62b615d40baf2692ce38a4879d4cc
Emeti <emetisuccess@gmail.com>
2022-05-15 01:35:59 +0100

    change

--- js/javascript.js
+++ js/javascript.js
@@ -1,264 +1,258 @@
-function bar() {
-    google.charts.load('current', {
-        'packages': ['bar']
-    });
-    google.charts.setOnLoadCallback(drawChart);
+google.charts.load('current', {
+    'packages': ['bar']
+});
+google.charts.setOnLoadCallback(drawBarChart);
 
-    function drawChart() {
-        var data = google.visualization.arrayToDataTable([
-            ['Year', 'Sales', 'Expenses', 'Profit'],
-            ['2014', 1000, 400, 200],
-            ['2015', 1170, 460, 250],
-            ['2016', 660, 1120, 300],
-            ['2017', 1030, 540, 350]
-        ]);
+function drawBarChart() {
+    var data = google.visualization.arrayToDataTable([
+        ['Year', 'Sales', 'Expenses', 'Profit'],
+        ['2014', 1000, 400, 200],
+        ['2015', 1170, 460, 250],
+        ['2016', 660, 1120, 300],
+        ['2017', 1030, 540, 350]
+    ]);
 
-        var options = {
-            chart: {
-                title: 'Company Performance',
-                subtitle: 'Sales, Expenses, and Profit: 2014-2017',
-            }
-        };
-
-        var chart = new google.charts.Bar(document.getElementById('columnchart_material'));
-
-        chart.draw(data, google.charts.Bar.convertOptions(options));
-    }
-
-}
-bar();
-
-function pie() {
-    google.charts.load('current', {
-        'packages': ['corechart']
-    });
-    google.charts.setOnLoadCallback(drawChart);
-
-    function drawChart() {
-
-        var data = google.visualization.arrayToDataTable([
-            ['Task', 'Hours per Day'],
-            ['Work', 11],
-            ['Eat', 2],
-            ['Commute', 2],
-            ['Watch TV', 2],
-            ['Sleep', 7]
-        ]);
-
-        var options = {
-            title: 'My Daily Activities'
-        };
-
-        var chart = new google.visualization.PieChart(document.getElementById('piechart'));
-
-        chart.draw(data, options);
-    }
-}
-pie();
-
-function lineChart() {
-    google.charts.load('current', {
-        'packages': ['line']
-    });
-    google.charts.setOnLoadCallback(drawChart);
-
-    function drawChart() {
-
-        var data = new google.visualization.DataTable();
-        data.addColumn('number', 'Day');
-        data.addColumn('number', 'Guardians of the Galaxy');
-        data.addColumn('number', 'The Avengers');
-        data.addColumn('number', 'Transformers: Age of Extinction');
-
-        data.addRows([
-            [1, 37.8, 80.8, 41.8],
-            [2, 30.9, 69.5, 32.4],
-            [3, 25.4, 57, 25.7],
-            [4, 11.7, 18.8, 10.5],
-            [5, 11.9, 17.6, 10.4],
-            [6, 8.8, 13.6, 7.7],
-            [7, 7.6, 12.3, 9.6],
-            [8, 12.3, 29.2, 10.6],
-            [9, 16.9, 42.9, 14.8],
-            [10, 12.8, 30.9, 11.6],
-            [11, 5.3, 7.9, 4.7],
-            [12, 6.6, 8.4, 5.2],
-            [13, 4.8, 6.3, 3.6],
-            [14, 4.2, 6.2, 3.4]
-        ]);
-
-        var options = {
-            chart: {
-                title: 'Box Office Earnings in First Two Weeks of Opening',
-                subtitle: 'in millions of dollars (USD)'
-            },
-            width: 700,
-            height: 450,
-            axes: {
-                x: {
-                    0: {
-                        side: 'top'
-                    }
-                }
-            }
-        };
-
-        var chart = new google.charts.Line(document.getElementById('line_top_x'));
-
-        chart.draw(data, google.charts.Line.convertOptions(options));
-    }
-}
-lineChart();
-
-function chart_div() {
-    google.charts.load('current', {
-        'packages': ['corechart']
-    });
-    google.charts.setOnLoadCallback(drawChart);
-
-    function drawChart() {
-        var data = google.visualization.arrayToDataTable([
-            ['Year', 'Sales', 'Expenses'],
-            ['2013', 1000, 400],
-            ['2014', 1170, 460],
-            ['2015', 660, 1120],
-            ['2016', 1030, 540]
-        ]);
-
-        var options = {
+    var options = {
+        chart: {
             title: 'Company Performance',
-            hAxis: {
-                title: 'Year',
-                titleTextStyle: {
-                    color: '#333'
-                }
-            },
-            vAxis: {
-                minValue: 0
-            }
-        };
+            subtitle: 'Sales, Expenses, and Profit: 2014-2017',
+        }
+    };
 
-        var chart = new google.visualization.AreaChart(document.getElementById('chart_div'));
-        chart.draw(data, options);
-    }
+    var chart = new google.charts.Bar(document.getElementById('columnchart_material'));
+
+    chart.draw(data, google.charts.Bar.convertOptions(options));
 }
-chart_div();
+
+
+google.charts.load('current', {
+    'packages': ['corechart']
+});
+google.charts.setOnLoadCallback(drawPieChart);
+
+function drawPieChart() {
+
+    var data = google.visualization.arrayToDataTable([
+        ['Task', 'Hours per Day'],
+        ['Work', 23],
+        ['Eat', 12],
+        ['Commute', 22],
+        ['Watch TV', 12],
+        ['Sleep', 37]
+    ]);
+
+    var options = {
+        title: 'My Daily Activities 2d'
+    };
+
+    var chart = new google.visualization.PieChart(document.getElementById('piechart'));
+
+    chart.draw(data, options);
+}
+
+
+
+google.charts.load('current', {
+    'packages': ['line']
+});
+google.charts.setOnLoadCallback(drawLineChart);
+
+function drawLineChart() {
+
+    var data = new google.visualization.DataTable();
+    data.addColumn('number', 'Day');
+    data.addColumn('number', 'Guardians of the Galaxy');
+    data.addColumn('number', 'The Avengers');
+    data.addColumn('number', 'Transformers: Age of Extinction');
+
+    data.addRows([
+        [1, 37.8, 80.8, 41.8],
+        [2, 30.9, 69.5, 32.4],
+        [3, 25.4, 57, 25.7],
+        [4, 11.7, 18.8, 10.5],
+        [5, 11.9, 17.6, 10.4],
+        [6, 8.8, 13.6, 7.7],
+        [7, 7.6, 12.3, 9.6],
+        [8, 12.3, 29.2, 10.6],
+        [9, 16.9, 42.9, 14.8],
+        [10, 12.8, 30.9, 11.6],
+        [11, 5.3, 7.9, 4.7],
+        [12, 6.6, 8.4, 5.2],
+        [13, 4.8, 6.3, 3.6],
+        [14, 4.2, 6.2, 3.4]
+    ]);
+
+    var options = {
+        chart: {
+            title: 'Box Office Earnings in First Two Weeks of Opening',
+            subtitle: 'in millions of dollars (USD)'
+        },
+        width: 700,
+        height: 450,
+        axes: {
+            x: {
+                0: {
+                    side: 'top'
+                }
+            }
+        }
+    };
+
+    var chart = new google.charts.Line(document.getElementById('line_top_x'));
+
+    chart.draw(data, google.charts.Line.convertOptions(options));
+}
+
+
+google.charts.load("current", {
+    packages: ['corechart']
+});
+google.charts.setOnLoadCallback(barChart);
 
 function barChart() {
+    var data = google.visualization.arrayToDataTable([
+        ["Element", "Density", {
+            role: "style"
+        }],
+        ["Copper", 8.94, "#b87333"],
+        ["Silver", 10.49, "silver"],
+        ["Gold", 19.30, "gold"],
+        ["Platinum", 21.45, "color: #e5e4e2"]
+    ]);
 
-    google.charts.load("current", {
-        packages: ['corechart']
-    });
-    google.charts.setOnLoadCallback(drawChart);
+    var view = new google.visualization.DataView(data);
+    view.setColumns([0, 1,
+        {
+            calc: "stringify",
+            sourceColumn: 1,
+            type: "string",
+            role: "annotation"
+        },
+        2
+    ]);
 
-    function drawChart() {
-        var data = google.visualization.arrayToDataTable([
-            ["Element", "Density", {
-                role: "style"
-            }],
-            ["Copper", 8.94, "#b87333"],
-            ["Silver", 10.49, "silver"],
-            ["Gold", 19.30, "gold"],
-            ["Platinum", 21.45, "color: #e5e4e2"]
-        ]);
-
-        var view = new google.visualization.DataView(data);
-        view.setColumns([0, 1,
-            {
-                calc: "stringify",
-                sourceColumn: 1,
-                type: "string",
-                role: "annotation"
-            },
-            2
-        ]);
-
-        var options = {
-            title: "Density of Precious Metals, in g/cm^3",
-            width: "100%",
-            height: 500,
-            bar: {
-                groupWidth: "95%"
-            },
-            legend: {
-                position: "none"
-            },
-        };
-        var chart = new google.visualization.ColumnChart(document.getElementById("columnchart_values"));
-        chart.draw(view, options);
-    }
-
+    var options = {
+        title: "Density of Precious Metals, in g/cm^3",
+        width: "100%",
+        height: 500,
+        bar: {
+            groupWidth: "95%"
+        },
+        legend: {
+            position: "none"
+        },
+    };
+    var chart = new google.visualization.ColumnChart(document.getElementById("columnchart_values"));
+    chart.draw(view, options);
 }
-barChart();
 
-function ganttChart() {
-    google.charts.load('current', {
-        'packages': ['gantt']
-    });
-    google.charts.setOnLoadCallback(drawChart);
 
-    function drawChart() {
 
-        var data = new google.visualization.DataTable();
-        data.addColumn('string', 'Task ID');
-        data.addColumn('string', 'Task Name');
-        data.addColumn('string', 'Resource');
-        data.addColumn('date', 'Start Date');
-        data.addColumn('date', 'End Date');
-        data.addColumn('number', 'Duration');
-        data.addColumn('number', 'Percent Complete');
-        data.addColumn('string', 'Dependencies');
+google.charts.load('current', {
+    'packages': ['gantt']
+});
+google.charts.setOnLoadCallback(drawGanttChart);
 
-        data.addRows([
-            ['2014Spring', 'Spring 2014', 'spring',
-                new Date(2014, 2, 22), new Date(2014, 5, 20), null, 100, null
-            ],
-            ['2014Summer', 'Summer 2014', 'summer',
-                new Date(2014, 5, 21), new Date(2014, 8, 20), null, 100, null
-            ],
-            ['2014Autumn', 'Autumn 2014', 'autumn',
-                new Date(2014, 8, 21), new Date(2014, 11, 20), null, 100, null
-            ],
-            ['2014Winter', 'Winter 2014', 'winter',
-                new Date(2014, 11, 21), new Date(2015, 2, 21), null, 100, null
-            ],
-            ['2015Spring', 'Spring 2015', 'spring',
-                new Date(2015, 2, 22), new Date(2015, 5, 20), null, 50, null
-            ],
-            ['2015Summer', 'Summer 2015', 'summer',
-                new Date(2015, 5, 21), new Date(2015, 8, 20), null, 0, null
-            ],
-            ['2015Autumn', 'Autumn 2015', 'autumn',
-                new Date(2015, 8, 21), new Date(2015, 11, 20), null, 0, null
-            ],
-            ['2015Winter', 'Winter 2015', 'winter',
-                new Date(2015, 11, 21), new Date(2016, 2, 21), null, 0, null
-            ],
-            ['Football', 'Football Season', 'sports',
-                new Date(2014, 8, 4), new Date(2015, 1, 1), null, 100, null
-            ],
-            ['Baseball', 'Baseball Season', 'sports',
-                new Date(2015, 2, 31), new Date(2015, 9, 20), null, 14, null
-            ],
-            ['Basketball', 'Basketball Season', 'sports',
-                new Date(2014, 9, 28), new Date(2015, 5, 20), null, 86, null
-            ],
-            ['Hockey', 'Hockey Season', 'sports',
-                new Date(2014, 9, 8), new Date(2015, 5, 21), null, 89, null
-            ]
-        ]);
+function drawGanttChart() {
 
-        var options = {
-            height: 400,
-            width: "100%",
-            gantt: {
-                trackHeight: 30
-            }
-        };
+    var data = new google.visualization.DataTable();
+    data.addColumn('string', 'Task ID');
+    data.addColumn('string', 'Task Name');
+    data.addColumn('string', 'Resource');
+    data.addColumn('date', 'Start Date');
+    data.addColumn('date', 'End Date');
+    data.addColumn('number', 'Duration');
+    data.addColumn('number', 'Percent Complete');
+    data.addColumn('string', 'Dependencies');
 
-        var chart = new google.visualization.Gantt(document.getElementById('gantt'));
+    data.addRows([
+        ['2014Spring', 'Spring 2014', 'spring',
+            new Date(2014, 2, 22), new Date(2014, 5, 20), null, 100, null
+        ],
+        ['2014Summer', 'Summer 2014', 'summer',
+            new Date(2014, 5, 21), new Date(2014, 8, 20), null, 100, null
+        ],
+        ['2014Autumn', 'Autumn 2014', 'autumn',
+            new Date(2014, 8, 21), new Date(2014, 11, 20), null, 100, null
+        ],
+        ['2014Winter', 'Winter 2014', 'winter',
+            new Date(2014, 11, 21), new Date(2015, 2, 21), null, 100, null
+        ],
+        ['2015Spring', 'Spring 2015', 'spring',
+            new Date(2015, 2, 22), new Date(2015, 5, 20), null, 50, null
+        ],
+        ['2015Summer', 'Summer 2015', 'summer',
+            new Date(2015, 5, 21), new Date(2015, 8, 20), null, 0, null
+        ],
+        ['2015Autumn', 'Autumn 2015', 'autumn',
+            new Date(2015, 8, 21), new Date(2015, 11, 20), null, 0, null
+        ],
+        ['2015Winter', 'Winter 2015', 'winter',
+            new Date(2015, 11, 21), new Date(2016, 2, 21), null, 0, null
+        ],
+        ['Football', 'Football Season', 'sports',
+            new Date(2014, 8, 4), new Date(2015, 1, 1), null, 100, null
+        ],
+        ['Baseball', 'Baseball Season', 'sports',
+            new Date(2015, 2, 31), new Date(2015, 9, 20), null, 14, null
+        ],
+        ['Basketball', 'Basketball Season', 'sports',
+            new Date(2014, 9, 28), new Date(2015, 5, 20), null, 86, null
+        ],
+        ['Hockey', 'Hockey Season', 'sports',
+            new Date(2014, 9, 8), new Date(2015, 5, 21), null, 89, null
+        ]
+    ]);
 
-        chart.draw(data, options);
-    }
+    var options = {
+        height: 400,
+        gantt: {
+            trackHeight: 30
+        }
+    };
+
+    var chart = new google.visualization.Gantt(document.getElementById('gantt'));
+
+    chart.draw(data, options);
 }
-ganttChart();
+
+
+
+google.charts.load("current", {
+    packages: ["corechart"]
+});
+google.charts.setOnLoadCallback(drawPie3dChart);
+
+function drawPie3dChart() {
+    var data = google.visualization.arrayToDataTable([
+        ['Task', 'Hours per Day'],
+        ['Work', 11],
+        ['Eat', 2],
+        ['Commute', 2],
+        ['Watch TV', 2],
+        ['Sleep', 7]
+    ]);
+
+    var options = {
+        title: 'My Daily Activities in 3d',
+        is3D: true,
+    };
+
+    var chart = new google.visualization.PieChart(document.getElementById('piechart_3d'));
+    chart.draw(data, options);
+}
+
+
+
+
+$(document).ready(function () {
+
+    $(window).resize(function () {
+        drawPie3dChart();
+        drawGanttChart();
+        barChart();
+        drawPieChart();
+        drawBarChart();
+    });
+
+})
